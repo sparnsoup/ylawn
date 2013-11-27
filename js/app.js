de0d62d1826394d26e13f5ff658f43609e0513d8
sparnsoup <bryan.seely@gmail.com>
2013-11-27 14:17:56 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,19 +3,3 @@ var APPLICATION_ID = 'E9A6DABE-9DBD-4A03-FFF9-F1807430F000',
     VERSION = 'v1'; //default application version;
 
 Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
-
-var user = new Backendless.User();
-user.email = "foo@foo.com";
-user.password = "foo";
-user.name = "Bob";
- 
-Backendless.UserService.register(user);
- 
-function Comment(args) {
-    args = args || {};
-    this.message = args.message || "";
-    this.authorEmail = args.authorEmail || "";
-}
-var dataStore = Backendless.Persistence.of(Comment);
-var commentObject = new Comment({message: "Hello, world!", authorEmail: user.email})  
-dataStore.save( commentObject );
