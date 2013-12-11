1961bf1c29f120d5e16ec51f43f3189d99b8e324
bryanseely <bseely@hollandamerica.com>
2013-12-11 14:57:04 -0800

    Wired up page1 to parse

--- js/app.js
+++ js/app.js
@@ -6,10 +6,76 @@ Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);*/
 
 Parse.initialize("vf6Psb1lB7Sge6xO3o8NLOHpdOB4ra2Xl9FZVmeV", "2Rpfp8ywh68TObCmzFlrj5NsUH7qnfWS5ZVEVsCu");
 
-var TestObject = Parse.Object.extend("TestObject");
+/*var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
 testObject.save({foo: "bar"}, {
   success: function(object) {
     alert("yay! it worked");
   }
+});*/
+
+var MowUpdate1 = Parse.Object.extend("MowUpdate1");
+var mowUpdate1 = new MowUpdate1();
+var mowQuery = new Parse.Query(MowUpdate1);
+var WaterUpdate1 = Parse.Object.extend("WaterUpdate1");
+var waterUpdate1 = new WaterUpdate1();
+var waterQuery = new Parse.Query(WaterUpdate1);
+var FertUpdate1 = Parse.Object.extend("FertUpdate1");
+var fertUpdate1 = new FertUpdate1();
+var fertQuery = new Parse.Query(FertUpdate1);
+
+//display saved data
+mowQuery.equalTo("user", "Bryan");
+mowQuery.find({
+    success: function (results) {
+        // Successfully retrieved the object.
+        for (var i = 0; i < results.length; i++) {
+            var mowObject = results[results.length - 1];
+            //alert(object.id + ' - ' + object.get('date'));
+            //var results = object;
+            var lastMowDate = mowObject.get('date');
+            var lastDirection = mowObject.get('direction');
+            $('#mow-date-field1').html(lastMowDate);
+            $('#mow-direction1').html(lastDirection);
+        }
+    },
+    error: function (error) {
+        alert("Error: " + error.code + " " + error.message);
+    }
+});
+
+//display saved data
+waterQuery.equalTo("user", "Bryan");
+waterQuery.find({
+    success: function (results) {
+        // Successfully retrieved the object.
+        for (var i = 0; i < results.length; i++) {
+            var waterObject = results[results.length - 1];
+            var lastWaterDate = waterObject.get('date');
+            $('#water-date-field1').html(lastWaterDate);
+        }
+    },
+    error: function (error) {
+        alert("Error: " + error.code + " " + error.message);
+    }
+});
+
+//display saved data
+fertQuery.equalTo("user", "Bryan");
+fertQuery.find({
+    success: function (results) {
+        // Successfully retrieved the object.
+        for (var i = 0; i < results.length; i++) {
+            var fertObject = results[results.length - 1];
+            //alert(object.id + ' - ' + object.get('date'));
+            //var results = object;
+            var lastFertDate = fertObject.get('date');
+            var lastType = fertObject.get('type');
+            $('#fert-date-field1').html(lastFertDate);
+            $('#fert-type1').html(lastType);
+        }
+    },
+    error: function (error) {
+        alert("Error: " + error.code + " " + error.message);
+    }
 });
